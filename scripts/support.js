d97c21bc093d4d81bb5c7eb0d2a1078701222f9d
StartOff123 <startoff123@mail.ru>
2024-04-11 01:06:30 +0500

    mobile fix

--- scripts/support.js
+++ scripts/support.js
@@ -1,3 +1,23 @@
+const phoneMobile = document.getElementById("phone-mobile");
+const careBot = document.getElementById("care-bot");
+
+window.addEventListener("load", () => {
+  const observerPhoneMobile = new IntersectionObserver(
+    (entries) => {
+      if (entries[0].isIntersecting) {
+        phoneMobile.classList.add("!translate-y-0", "!opacity-100");
+      } else {
+        phoneMobile.classList.remove("!translate-y-0", "!opacity-100");
+      }
+    },
+    {
+      threshold: 0.7,
+    }
+  );
+
+  observerPhoneMobile.observe(careBot);
+});
+
 const antiCounterfeitProtectionPopup = new Popup(
   "anti-counterfeit-protection-popup",
   {
